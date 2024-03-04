2a1acb45394964a82af698a39c980a8dc3f3a2cd
Voxxin <59192970+Voxxin@users.noreply.github.com>
2024-03-03 21:50:08 -0500

    Initial

---
+++ src/main/java/com/github/voxxin/shp/ScreenHelperAPI.java
@@ -0,0 +1,9 @@
+package com.github.voxxin.shp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScreenHelperAPI implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
